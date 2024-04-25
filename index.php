190cb252d11b5e197f04784e5d4ed66e423b4ba8
RishatRyan339 <70919292+RishatRyan339@users.noreply.github.com>
2024-04-26 01:42:44 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,421 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+
+
+    
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swiper/swiper-bundle.min.css"
+    />
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="script.js"></script>
+<!-- CSS LINK  -->
+
+<link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">
+        <img src="./img/1.png" alt="logo" height="43px" width="64px">
+        
+        </a>
+    
+
+
+    <nav class="navbar" id="navbar1">
+            <a href="#cart"><i class='fas fa-shopping-cart' style='font-size:18px'></i>shopping cart</a>
+            <a href="#men">my wish list</a>
+            <a href="#women">create account</a>
+            
+            
+            
+        </nav>
+    
+        <input type="text" placeholder="Search..">
+        <button type="submit"><i class="fas fa-search"></i></button>
+    </header>
+
+    <header class="header2">
+
+    <nav class="navbar2" id="navbar2">
+            <a href="#home">home</a>
+            <a href="#men">men</a>
+            <a href="#women">women</a>
+            <a href="#kids">kids</a>
+            
+            
+        </nav>
+</header>
+
+<!-- ----------------------------------- --- ---                           JS SLIDER                                 ------------------------------>
+<div class="swiper mySwiper">
+<div class="swiper-wrapper">
+   
+  <div class="swiper-slide" id="swiper-slide1">
+     <!-- <img src="images/firsticon.jpg"><br>  -->
+     <div class="layer">
+
+        <div class="text"> 
+</p> 
+<br> <a href="#" class="btn"></a>
+         </div>
+    
+    </div>
+  </div>
+
+
+    <div class="swiper-slide"id="swiper-slide2">
+      
+      <div class="layer">
+      <div class="text"> <h1></h1><br><p>
+      </p> 
+      <br> <a href="#" class="btn"></a></div>
+          
+    </div>
+          </div>
+
+   
+          <div class="swiper-slide"id="swiper-slide3">
+      
+      <div class="layer">
+      <div class="text"> <h1></h1><br><p>
+      </p> 
+      <br> <a href="#" class="btn"></a></div>
+          
+    </div>
+          </div>
+
+   
+          <div class="swiper-slide"id="swiper-slide4">
+      
+      <div class="layer">
+      <div class="text"> <h1></h1><br><p>
+      </p> 
+      <br> <a href="#" class="btn"></a></div>
+          
+    </div>
+          </div>
+
+   
+   
+
+
+    <div class="swiper-slide"id="swiper-slide5">
+      <div class="text"> 
+        
+
+     
+      <br> <a href="#" class="btn">Reserve</a></div>
+          
+          
+          </div>
+
+    </div>
+   
+
+  <div class="swiper-button-next"></div>
+  <div class="swiper-button-prev"></div>
+
+</div>
+<!-------------------------------------------------------------------------------- SECOND WRAPPER ---------------------------------------------------------------------------------------------------------->
+<!-- <section class="secondher">
+
+<div class="wrapper">
+  <input checked type=radio name="slider" id="slide1" />
+  <input type=radio name="slider" id="slide2" />
+  <input type=radio name="slider" id="slide3" />
+  <input type=radio name="slider" id="slide4" />
+  <input type=radio name="slider" id="slide5" />
+
+  <div class="slider-wrapper">
+    <div class=inner>
+      <article>
+        <div class="info top-left">
+          <h3>Malacca</h3></div>
+        <img src="https://farm9.staticflickr.com/8059/28286750501_dcc27b1332_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-right">
+          <h3>Cameron Highland</h3></div>
+        <img src="https://farm6.staticflickr.com/5812/23394215774_b76cd33a87_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-left">
+          <h3>New Delhi</h3></div>
+        <img src="https://farm8.staticflickr.com/7455/27879053992_ef3f41c4a0_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info top-right">
+          <h3>Ladakh</h3></div>
+        <img src="https://farm8.staticflickr.com/7367/27980898905_72d106e501_h_d.jpg" />
+      </article>
+
+      <article>
+        <div class="info bottom-left">
+          <h3>Nubra Valley</h3></div>
+        <img src="https://farm8.staticflickr.com/7356/27980899895_9b6c394fec_h_d.jpg" />
+      </article>
+    </div>
+    <!-- .inner -->
+  </div>
+  <!-- .slider-wrapper -->
+
+  <!-- <div class="slider-prev-next-control">
+    <label for=slide1></label>
+    <label for=slide2></label>
+    <label for=slide3></label>
+    <label for=slide4></label>
+    <label for=slide5></label>
+  </div>
+  .slider-prev-next-control -->
+
+  <!-- <div class="slider-dot-control">
+    <label for=slide1></label>
+    <label for=slide2></label>
+    <label for=slide3></label>
+    <label for=slide4></label>
+    <label for=slide5></label>
+  </div>  -->
+  <!-- .slider-dot-control -->
+</div>
+</section> 
+<div class="third-layer">
+<h1 class="third-heading">
+  Live your fashion, <br> live your class<br>
+
+</h1>
+
+<div class="btn"id="btn">
+            <div class="eff-2"></div>
+           <a href="#">Shop Now</a>
+        </div>
+  </div>
+<section class="third">
+
+  <div class="poster">
+  
+
+<img src="">
+</div>
+</section>
+<section class="fourth">
+  <h1 class="heading"id="fourth-heading">Panjabi Collection</h1>
+<div class="swiper1 mySecondSwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"id="swiper0">
+   <div class="btn-card">Click</div>
+      <div class="paragraph">
+    <h3 class="Item">Panjabi</h3>
+    <p id="price">2,550 BDT</p>
+   </div>
+     
+        
+
+    </div>
+      <div class="swiper-slide" id="swiper1">  <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide" id="swiper2">
+
+        <img id="swiper2img" src="https://www.texmartonline.com/wp-content/uploads/2023/03/03796-1.webp"/>
+      </div>
+      <div class="swiper-slide"id="swiper3"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper4"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper5"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper6"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper7"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper8"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+    </div>
+  
+  </div>
+</section>
+
+<div class="fifth-layer">
+
+<h1 class="fifth-heading">
+  Live your fashion, <br> live your class<br>
+
+</h1>
+
+<div class="btn2"id="btn2">
+            <div class="eff-2"></div>
+           <a href="#">Shop Now</a>
+        </div>
+  </div>
+</div>
+<section class="fifth">
+  
+<div class="poster">
+  
+
+  <img src="">
+  </div>
+</section>
+
+<section class="sixth">
+  <h1 class="heading"id="fourth-heading">Women's  Collection</h1>
+<div class="swiper2 myThirdSwiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"id="swiper10">
+   <div class="btn-card">Click</div>
+      <div class="paragraph">
+    <h3 class="Item">Panjabi</h3>
+    <p id="price">2,550 BDT</p>
+   </div>
+     
+        
+
+    </div>
+      <div class="swiper-slide" id="swiper11">  <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide2" id="swiper12">
+
+        <img id="swiper2img" src="https://www.texmartonline.com/wp-content/uploads/2023/03/03796-1.webp"/>
+      </div>
+      <div class="swiper-slide"id="swiper13"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper14"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper15"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper16"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper17"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+      <div class="swiper-slide"id="swiper18"> <div class="paragraph">
+        <h3 class="Item">Panjabi</h3>
+        <p id="price">2,550 BDT</p>
+       </div>
+         </div>
+    </div>
+  
+  </div>
+</section>
+
+<div class="seventh-layer">
+
+<h1 class="seventh-heading">
+  Live your fashion, <br> live your class<br>
+
+</h1>
+
+<div class="btn2"id="btn2">
+            <div class="eff-2"></div>
+           <a href="#">Shop Now</a>
+        </div>
+  </div>
+</div>
+
+<section class="seventh">
+  
+<div class="poster">
+  
+
+  <img src="">
+  </div>
+</section>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+<!-- Initialize Swiper -->
+<script>
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var secondSwiper = new Swiper(".mySecondSwiper", {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 50,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+    delay: 2000, // Delay between transitions (in milliseconds)
+    disableOnInteraction: false, // Disable autoplay when user interacts with slider
+  },
+    });
+
+    var thirdSwiper = new Swiper(".myThirdSwiper", {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 50,
+      grabCursor: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+    delay: 2000, // Delay between transitions (in milliseconds)
+    disableOnInteraction: false, // Disable autoplay when user interacts with slider
+  },
+    });
+</script>
+</body>
+</html>
